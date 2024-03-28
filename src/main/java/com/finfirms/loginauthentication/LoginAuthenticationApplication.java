05caf5f40f118a4aa6fe85fd7523929430d119d7
Aravind <aravindmunna240@gmail.com>
2024-03-28 09:46:53 +0530

    Initial commit

---
+++ src/main/java/com/finfirms/loginauthentication/LoginAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.finfirms.loginauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finfirms/loginauthentication/LoginAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finfirms.loginauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
